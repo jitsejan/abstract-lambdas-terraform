82dfc261dd887ca87d13a82afefa69857080bcea
Jitse-Jan <code@jitsejan.com>
2019-07-20 20:35:15 +0100

    IGDBApiResolver class

--- __main__.py
+++ __main__.py
@@ -3,39 +3,47 @@ import json
 import os
 import requests
 
-BASE_URL = 'https://api-v3.igdb.com/{endpoint}'
-HEADERS = {
-    'user-key': os.environ.get('IGDB_KEY', '')
-}
 
+class IGDBApiResolver:
+    """ Class definition for the IGDBApiResolver """
+    BASE_URL = 'https://api-v3.igdb.com/{endpoint}'
+    HEADERS = {
+        'user-key': os.environ.get('IGDB_KEY', '')
+    }
 
-def _get_games(platform_id, name=None):
-    """ Get the games for a given platform and an optional name filter """
-    endpoint = 'games'
-    if name:
-        query = 'fields name; where name ~ *"{name}"* & platforms = {platform_id}; limit 50;'
-    else:
-        query = 'fields name; where platforms = {platform_id}; limit 50;'
-    response = requests.get(BASE_URL.format(endpoint=endpoint),
-                            data=query.format(name=name, platform_id=platform_id),
-                            headers=HEADERS)
-    return response.json()
+    def _get_api_json_response(self, endpoint, data):
+        """ Return the API JSON response """
+        response = requests.get(url=self.BASE_URL.format(endpoint=endpoint),
+                                data=data,
+                                headers=self.HEADERS)
+        return response.json()
 
+    def get_games(self, platform_id, name=None):
+        """ Get the games for a given platform and an optional name filter """
+        endpoint = 'games'
+        if name:
+            query = 'fields name; where name ~ *"{name}"* & platforms = {platform_id}; limit 50;'
+        else:
+            query = 'fields name; where platforms = {platform_id}; limit 50;'
 
-def _get_platform_id(platform_abbreviation):
-    """ Get the platform ID for a given platform abbreviation """
-    endpoint = 'platforms'
-    query = 'fields id; where abbreviation = "{abbr}";'
-    response = requests.get(BASE_URL.format(endpoint=endpoint),
-                            data=query.format(abbr=platform_abbreviation),
-                            headers=HEADERS)
-    return response.json()[0]["id"]
+        return self._get_api_json_response(endpoint=endpoint,
+                                           data=query.format(name=name,
+                                                             platform_id=platform_id))
+
+    def get_platform_id(self, platform_abbreviation):
+        """ Get the platform ID for a given platform abbreviation """
+        endpoint = 'platforms'
+        query = 'fields id; where abbreviation = "{abbr}";'
+
+        return self._get_api_json_response(endpoint=endpoint,
+                                           data=query.format(abbr=platform_abbreviation))[0]['id']
 
 
 def main():
     """ Main function """
-    platform_id = _get_platform_id("N64")
-    games = _get_games(platform_id, "Mario")
+    game_api = IGDBApiResolver()
+    platform_id = game_api.get_platform_id("N64")
+    games = game_api.get_games(platform_id, "Mario")
     print(json.dumps(games))
 
 
