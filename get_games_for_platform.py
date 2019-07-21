7713b6ad76e3397613eb272dc4d01285c26632fc
Jitse-Jan <code@jitsejan.com>
2019-07-22 00:56:18 +0100

    Added Lambda functions

---
+++ get_games_for_platform.py
@@ -0,0 +1,22 @@
+""" get_games_for_platform.py """
+import json
+from igdbapiresolver import IGDBApiResolver
+
+
+def lambda_handler(event, context):
+    """ Main function """
+    game_api = IGDBApiResolver()
+    platform_id = game_api.get_data(
+        endpoint="get_platform_id", abbr=event.get("platform_abbr", "")
+    )[0]["id"]
+    games = game_api.get_data(
+        endpoint="get_games_for_platform_with_name",
+        platform_id=platform_id,
+        name=event.get("name", ""),
+    )
+    print(json.dumps(games))
+
+
+if __name__ == "__main__":
+    event = {"platform_abbr": "N64", "name": "Mario"}
+    lambda_handler(event, {})
---
+++ get_programming_joke.py
@@ -0,0 +1,13 @@
+""" get_programming_joke.py """
+import json
+from jokeapiresolver import JokeApiResolver
+
+
+def lambda_handler(event, context):
+    """ Main function """
+    joke_api = JokeApiResolver()
+    print(json.dumps(joke_api.get_data(endpoint="get_programming_joke")))
+
+
+if __name__ == "__main__":
+    lambda_handler({}, {})
